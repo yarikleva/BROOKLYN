51881cd4c2f04462bd192c7d0e370c78ed0f3e72
yarikleva <levchenko153729@gmail.com>
2024-01-09 15:41:27 +0200

    fix2

--- js/gallery-swiper.js
+++ js/gallery-swiper.js
@@ -2,10 +2,6 @@ const swiper = new Swiper('.gallery-swiper', {
     loop: true,
     slidesPerView:2.5,
     spaceBetween: 32,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
     autoplay:{
         delay: 2000,
     },
